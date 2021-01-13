974240642fe06cc68ba7a8fe4f6a5232921be8a3
unknown <mohamed.benighil07@gmail.com>
2021-01-13 22:33:58 +0100

    Create User model and  it's validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const express = require("express")
 const router = express.Router()
+const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
 
 // @route /api/users
 // @desc Register a user
 // @access Public
-router.post('/',(req,res) => {
-    res.send("User regestred")
+router.post('/',  
+body('name','Please enter a Name ').not().isEmpty(),
+body('email','Please enter an email').isEmail(),
+body('password', 'Please enter a password at least 5 chars').isLength({ min: 5 }),
+(req,res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    res.send("Passed")
 })
 
 module.exports = router
