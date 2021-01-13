f947835383c4593bb0a6a8bfe95072a5a30e64ae
unknown <mohamed.benighil07@gmail.com>
2021-01-13 23:55:34 +0100

    Save a user to DB with hashed password

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 const { body, validationResult } = require('express-validator');
 
 
@@ -11,12 +12,41 @@ router.post('/',
 body('name','Please enter a Name ').not().isEmpty(),
 body('email','Please enter an email').isEmail(),
 body('password', 'Please enter a password at least 5 chars').isLength({ min: 5 }),
-(req,res) => {
+async (req,res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
-    res.send("Passed")
+    //get client input
+    const {name, email, password} = req.body
+
+    try {
+      // check if user exists
+      let user = await User.findOne({email})
+      //yes
+      if (user){
+          return res.status(400).json({msg: 'User already exists'})
+      }
+      //no
+      user = new User({
+          name,
+          email,
+          password
+      })
+      //generate a salt (key)
+      const salt = await bcrypt.genSalt(10)
+      //hash password
+      user.password = await bcrypt.hash(password, salt)
+      //save to db
+      await user.save()
+      res.send('User saved')
+
+    } catch (error) {
+        //print error (for devlopper)
+        console.log(error.message)
+        //send a message for client
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router
