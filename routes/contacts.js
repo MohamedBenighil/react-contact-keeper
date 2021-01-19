12d718400e0ffb71a422efb8f3a1899b3e2b16ae
unknown <mohamed.benighil07@gmail.com>
2021-01-19 22:24:24 +0100

    Update a contact to specific user

--- routes/contacts.js
+++ routes/contacts.js
@@ -34,27 +34,73 @@ async (req,res) =>{
     // notice we have to pass user attribute
     const newContact = Contact({ name, user, phone, type, user: req.user.id})
     try {
-        //notive contact object
+        //notice contact object
         const contact = await newContact.save()
-        res.status(200).json({contact})
+        res.json({contact})
     } catch (err) {
         console.log(err.message)
         res.status(500).send('Server Error')
     }
 })
 
-//@route /api/contacts
-//@desc delete contact
-//@access Private
-router.delete('/:id', (req,res) =>{
-    res.send('Contact delete')
-})
 
 //@route /api/contacts
 //@desc update contact
 //@access Private
-router.put('/:id', (req,res) =>{
-    res.send('Contact updated')
+router.put('/:id', auth ,async (req,res) =>{
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        //is contact with that id exists ?
+        let contact = await Contact.findById(req.params.id)
+        //no
+        if(!contact){
+            res.status(404).json({msg: "Contact not found"})
+        }
+        // yes
+        // but is it belongs to that user ?
+        // no
+        if(contact.user.toString()!== req.user.id){
+            res.status(401).json({msg: "No authorization"})
+        }
+        //yes
+        //get the fields
+        const {name, type,phone, email} = req.body
+        //create object with that fields
+        const contactFields = {}
+        if (name) contactFields.name = name
+        if (email) contactFields.email = email
+        if (phone) contactFields.phone = phone
+        if (type) contactFields.type = type
+        //update the db        
+        contact = await Contact.findByIdAndUpdate(req.params.id, {$set: contactFields}, {new: true})
+        res.json(contact)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
 })
+
+
+
+
+
+
+
+
+
+//@route /api/contacts
+//@desc delete contact
+//@access Private
+router.delete('/:id', auth, async (req,res) =>{
+
+    
+})
+
+
 
 module.exports = router
