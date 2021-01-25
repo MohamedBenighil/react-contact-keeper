b1d9c43026ab84e48c29a9588f97782debdb172f
unknown <mohamed.benighil07@gmail.com>
2021-01-26 00:10:31 +0100

    Create ContactReducer

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, {useReducer} from 'react'
-import uuid from 'uuid'
+import {v4} from 'uuid'
 import ContactContext from './contactContext'
 import ContactReducer from './contactReducer'
 import {
@@ -41,25 +41,32 @@ const ContactState = props => {
     }
 
     const [state, dispatch] =  useReducer(ContactReducer, initialSate)
-        
-        //Add Contact
-        
-        //Delete Contact
-        
-        //Set Current Contact
-        
-        //Clear Current Contact 
-        
-        //Update Contact 
-        
-        //Filter Contacts 
-        
-        //Clear Filter
 
+    //Add Contact
+    const addContact = contact => {
+        contact.id = 15
+        dispatch({
+            type: ADD_CONTACT,
+            payload: contact 
+        })
+    }
+    
+    //Delete Contact
+    
+    //Set Current Contact
+    
+    //Clear Current Contact 
+    
+    //Update Contact 
+    
+    //Filter Contacts 
+    
+    //Clear Filter
     return (
         <ContactContext.Provider
         value={{
-            contacts : state.contacts
+            contacts : state.contacts,
+            addContact
         }}
         >
         {props.children}
